76cc365e68d929a552cf7b153b08bd5176c941a2
mike <62441770+rumintsev@users.noreply.github.com>
2023-11-14 14:13:21 +0300

    Update 2.py

--- 14.11.23/2.py
+++ 14.11.23/2.py
@@ -5,7 +5,6 @@ def mcm(dims):
     c = [[0 for x in range(n + 1)] for y in range((n + 1))]
 
     for l in range(2, n + 1):   
-
         for i in range(1, n - l + 2):
             j = i + l - 1
             c[i][j] = sys.maxsize
